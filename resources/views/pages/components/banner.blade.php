9864fb9d214ff803b3620e1a7986395ed1878858
anderoma <antoine.deroma@gmail.com>
2020-02-14 16:04:43 +0100

    text_center_banner

--- resources/views/pages/components/banner.blade.php
+++ resources/views/pages/components/banner.blade.php
@@ -4,9 +4,9 @@
     alt="Banner" height="auto" width="100%">
     
     <div class="carousel-caption">
-      <div class="container d-flex flex-column">
-        <div class="row align-items-center justify-content-end" style="min-height: 550px;">
-          <div class="col-12 text-center text-white">
+      <div class="container d-flex flex-column mb-5">
+        <div class="row align-items-center justify-content-end mb-5 pb-5" style="min-height: 550px;">
+          <div class="col-12 text-center text-white mb-5 pb-5">
             <h1 class="display-1">Porter une création</h1>
             <h4 class="mb-0">Fabrication artisanale</h4>
           </div>
