c1c8af78570befdae59824f0e9a8ccad0f4e9d01
anderoma <antoine.deroma@gmail.com>
2020-02-14 16:00:54 +0100

    about

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,75 @@
+@extends ('layout.layout')
+
+@section ('content')
+  <div class="container">
+    <!-- BREADCRUMB -->
+    <nav class="my-5">
+      
+        <div class="row">
+          <div class="col-12">
+
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb mb-0 font-size-xs text-gray-400">
+              <li class="breadcrumb-item">
+                <a class="text-gray-400" href="index.html">Home</a>
+              </li>
+              <li class="breadcrumb-item active">
+                About Us
+              </li>
+            </ol>
+
+          </div>
+        </div>
+      
+    </nav>
+
+    <!-- BANNER -->
+    
+      <div class="carousel-inner" style="height:500px">
+        <div class="carousel-item active">
+          <img src="https://images.pexels.com/photos/1884583/pexels-photo-1884583.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            alt="Banner" height="800px" width="auto">
+            
+          <div class="carousel-caption">
+            <div class="container d-flex flex-column">
+              <div class="row align-items-center justify-content-end" style="min-height: 550px;">
+                <div class="col-12 text-center text-white">
+                  <h1 class="mb-0 text-center text-white">
+                    We believe we can all make a stylish.
+                  </h1>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+
+    <div class="text-center mt-5">
+
+      <!-- Preheading -->
+      <h4 class="heading-xxs mb-3 text-gray">
+        Our story
+      </h6>
+
+      <!-- Heading -->
+      <h2 class="mb-7">About our Store</h2>
+
+      <!-- Text -->
+      <p class="font-size-lg text-muted">
+        Open created shall two he second moving whose. He face
+        whose two upon, fowl behold waters. Fly there their day
+        creepeth. Darkness beginning spirit after. Creepeth subdue.
+        Brought may, first. Under living that.
+      </p>
+      <p class="font-size-lg text-muted">
+        Third. For morning whales saw were had seed can't divide
+        it light shall moveth, us blessed given wherein.
+      </p>
+
+        
+    </div>
+    </div>
+
+@endsection
