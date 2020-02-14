e57762ba901151e9bc7282a0d1f7f096607ec315
anderoma <antoine.deroma@gmail.com>
2020-02-14 10:23:55 +0100

    components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,42 +11,20 @@
     <script src="/bootstrap/js/jquery.min.js"></script>
     <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <link href="css/e_commerce.css" rel="stylesheet">
+    <link href="/css/e_commerce.css" rel="stylesheet">
   </head>
   <body>
     <header>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      @component('components.navbar')
+      @endcomponent
 
     </header>
 
     @yield ('content')
 
-    <footer class="footer py-4 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; E-commerce 2020</p>
-      </div>
-    </footer>
+    @component('components.footer')
+    @endcomponent
+    
   </body>
 </html>
