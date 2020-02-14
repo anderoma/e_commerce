e5e0d51bcee1d7c0a300d06e786da2841d55c184
anderoma <antoine.deroma@gmail.com>
2020-02-14 16:30:51 +0100

    t

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,22 +4,20 @@
   <div class="container">
     <!-- BREADCRUMB -->
     <nav class="my-5">
-      
-        <div class="row">
-          <div class="col-12">
+      <div class="row">
+        <div class="col-12">
+          <!-- Breadcrumb -->
+          <ol class="breadcrumb mb-0 font-size-xs text-gray-400">
+            <li class="breadcrumb-item">
+              <a class="text-gray-400 no-decoration" href="index.html">Home</a>
+            </li>
+            <li class="breadcrumb-item active">
+              About Us
+            </li>
+          </ol>
 
-            <!-- Breadcrumb -->
-            <ol class="breadcrumb mb-0 font-size-xs text-gray-400">
-              <li class="breadcrumb-item">
-                <a class="text-gray-400" href="index.html">Home</a>
-              </li>
-              <li class="breadcrumb-item active">
-                About Us
-              </li>
-            </ol>
-
-          </div>
         </div>
+      </div>
       
     </nav>
 
