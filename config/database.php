1b7509ba6c9dfd226cb2d318a8ca271ee2273078
anderoma <antoine.deroma@gmail.com>
2020-02-17 18:08:42 +0100

    for_local

--- config/database.php
+++ config/database.php
@@ -4,10 +4,17 @@ use Illuminate\Support\Str;
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+if (empty($host)) {
+    $host = env('DB_HOST', '127.0.0.1');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
+} else {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+}
 
 return [
 
