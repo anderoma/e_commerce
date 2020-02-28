7c6c4e24cf759bb29b546b4b1a40f11ae1ac83a4
anderoma <antoine.deroma@gmail.com>
2020-02-28 15:46:17 +0100

    l

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,33 +1,29 @@
 @extends ('layout.layout')
 
 @section ('content')
-  <div class="container">
-    <h3>Produits</h3>
-    <div class="agile_top_brands_grids">
+  <div class="container py-5">
+    <h3 class="text-center">Produits</h3>
+    <div class="row">
       @foreach($products as $product)
-      <div class="col-md-3 top_brand_left">
-        <div class="hover14 column">
-          <div class="agile_top_brand_left_grid">
-            <div class="tag"><img src="images/tag.png" alt=" " class="img-responsive" /></div>
-              <div class="agile_top_brand_left_grid1">
-                <figure>
-                  <div class="snipcart-item block" >
-                    <div class="snipcart-thumb">
-                      <a href="{!! route('product.show',$product->id) !!}"><img width="150" height="150" title=" " alt=" " src="{!! $product->image !!}" /></a>
-                      <p>{!! $product->title !!}</p>
-                      <h4>${!! $product->discount_price !!} <span>${!! $product->original_price !!}</span></h4>
-          
-                      <div class="snipcart-details top_brand_home_details">
-                        <a class="btn btn-primary" href="#">Add to Cart</a>
-                      </div>
-                    </div>
-                  </div>
-                </figure>
+      <div class="col-md-4">
+        <div class="card mb-4 shadow-sm">
+
+          <div class="bd-placeholder-img card-img-top"><img src="images/tag.png" alt=" " class="img-responsive" /></div>
+            
+            <div class="card-body" >
+      
+              <a href="{!! route('product.show',$product->id) !!}"><img width="150" height="150" title=" " alt=" " src="{!! $product->image !!}" /></a>
+              <p class="card-text">{!! $product->title !!}</p>
+              <h4>${!! $product->discount_price !!} <span>${!! $product->original_price !!}</span></h4>
+            
+              <div class="btn-group">
+                <a class="btn btn-primary" href="#">Add to Cart</a>
               </div>
             </div>
           </div>
         </div>
       </div>
+    
       @endforeach
       </div>
       </div>
